844251a02ba1057e8da60b30427f4b50eb652462
aperire <aperire0402@gmail.com>
2022-08-21 18:00:41 +0900

    Add libertee-client

---
+++ src/libertee-client.js
@@ -0,0 +1,182 @@
+const { ethers } = require("ethers");
+const { contractData } = require("./contract");
+const pinataSDK = require('@pinata/sdk');
+const fs = require("fs");
+
+class Libertee {
+    /*
+    @dev Initialize Contract
+    */
+    constructor(PRIVATE_KEY, PROVIDER) {
+        this.provider = ethers.getDefaultProvider(PROVIDER);
+        this.signer = new ethers.Wallet(PRIVATE_KEY, this.provider);
+        this.contract = new ethers.Contract(
+            contractData.address,
+            contractData.abi,
+            this.signer
+        );
+    }
+
+    /*
+    @dev Upload to ipfs
+    */
+    uploadPinata = async(filePath, name, PINATA_KEY, PINATA_SECRET) => {
+        const pinata = pinataSDK(PINATA_KEY, PINATA_SECRET);
+        const readableStreamForFile = fs.createReadStream(filePath);
+        const options = {
+            pinataMetaData: {
+                name: name
+            },
+            pinataOptions: {
+                cidVersion: 0
+            }
+        };
+        const result = await pinata.pinFileToIPFS(readableStreamForFile, options);
+        const hash = result.IpfsHash;
+        return hash;
+    }
+
+    /*
+    @dev Call Methods
+    */
+    getMediaArrayLength = async() =>{
+        const mediaArrayLength = await this.contract.getMediaArrayLength();
+        return parseInt(mediaArrayLength);
+    }
+
+    getNameArrayLength = async() =>{
+        const nameArrayLength = await this.contract.getNameArrayLength();
+        return parseInt(nameArrayLength);
+    }
+    
+    getOwnerMediaLength = async(address) =>{
+        const ownerMediaLength = await this.contract.getOwnerMediaLength(address);
+        return parseInt(ownerMediaLength);
+    }
+    
+    getHashTagMediaLength = async(hashTag) => {
+        const hashTagMediaLength = await this.contract.getHashTagMediaLength(hashTag);
+        return parseInt(hashTagMediaLength);
+    }
+    
+    getMediaArray = async(index) => {
+        const media = await this.contract.mediaArray(index);
+        return media
+    }
+    
+    getNameArray = async(index) => {
+        const name = await this.contract.nameArray(index);
+        return name
+    }
+    
+    getMediaOwnershipMap = async(address, index) => {
+        const ownerMedia = this.contract.mediaOwnershipMap(address, index);
+        return ownerMedia;
+    }
+    
+    getProfileMap = async(address) => {
+        const profile = this.contract.profileMap(address);
+        return profile;
+    }
+    
+    getProfileNameMap = async(name) => {
+        const profile = this.contract.profileNameMap(name);
+        return profile;
+    }
+    
+    getHashTagMap = async(hashTag, index) => {
+        const media = this.contract.hashTagMap(hashTag, index);
+        return media
+    }
+    
+    /*
+    @dev Transaction Methods
+    */
+
+    createAccount = async(
+        pfpHash,
+        nickName,
+        bio,
+        telegram,
+        twitter,
+        phone,
+        email,
+        website,
+        hashTagArray
+    ) => {
+        const tx = await this.contract.createAccount(
+            pfpHash,
+            nickName,
+            bio,
+            telegram,
+            twitter,
+            phone,
+            email,
+            website,
+            hashTagArray
+        );
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editPfpHash = async(pfpHash) => {
+        const tx = await this.contract.editPfpHash(pfpHash);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editBio = async(bio) => {
+        const tx = await this.contract.editBio(bio);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editTelegram = async(telegram) => {
+        const tx = await this.contract.editTelegram(telegram);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editTwitter = async(twitter) => {
+        const tx = await this.contract.editTwitter(twitter);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editEmail = async(email) => {
+        const tx = await this.contract.editEmail(email);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    editHashTagArray = async(hashTagArray) => {
+        const tx = await this.contract.editHashTagArray(hashTagArray);
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+    postMedia = async(
+        ipfsHash,
+        text,
+        hashTag
+    ) => {
+        const tx = await this.contract.postMedia(
+            ipfsHash,
+            text,
+            hashTag
+        );
+        const txHash = await tx.hash;
+        await tx.wait();
+        return txHash;
+    }
+
+}
+
+module.exports = { Libertee };
